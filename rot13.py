6903c403969a6012296779cea6dc75ae8ef514d5
direwolf <stonelds@163.com>
2016-05-14 17:16:08 +0800

    init

---
+++ rot13.py
@@ -0,0 +1,5 @@
+import string
+rot13 = string.maketrans( 
+    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
+    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+print string.translate("FG3TRV22BUN7Q", rot13)
